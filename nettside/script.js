ee7225244885383460ec1785ac83c5fb514a11fc
Fridtjof Hegna Letting <Fridtjof.letting@soprasteria.com>
2016-07-12 13:15:39 +0200

    Funksjon for inportering av json og nedlasting av fil

---
+++ nettside/script.js
@@ -0,0 +1,53 @@
+﻿
+
+(function () {
+    var textFile = null,
+        makeTextFile = function (text) {
+            var data = new Blob([text], { type: 'text/plain' });
+
+            // If we are replacing a previously generated file we need to
+            // manually revoke the object URL to avoid memory leaks.
+            if (textFile !== null) {
+                window.URL.revokeObjectURL(textFile);
+            }
+
+            textFile = window.URL.createObjectURL(data);
+
+            return textFile;
+        };
+
+
+    var create = document.getElementById('create'),
+        textbox = document.getElementById('leseText');
+
+    create.addEventListener('click', function () {
+        var link = document.getElementById('downloadlink');
+        link.href = makeTextFile(textbox.value);
+        link.style.display = 'block';
+    }, false);
+})();
+
+$(document).ready(function () {
+    $.ajaxSetup({cache: false})
+    $("#leseFil").click(function () {
+        $.getJSON('http://tfbrowser.routable.org/api', function (result) {
+            console.log(result);
+            //document.write(result.name); skriver til hovedside etter "last inn"
+            $("#leseText").write(result.name);
+          
+            //content = "";
+            // content2 = "";
+            // content += result.name + "\n";
+            // content += result.description + "\n";
+            // content2 += result.variables[0].name;
+            // content2 += result.variables[0].description;
+            //content2 = result;
+            //$("#leseText").html(content);
+               // $.each(result, function (i, field) {
+              //      $("#leseText").append(field + " ");
+              
+            //});
+        });
+    });
+});
+
