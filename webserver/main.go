6919215595868e5e7151d615a0c42641d0451fd9
Martin Jensen <martin.jensen.91@gmail.com>
2016-07-12 13:19:42 +0200

    adds documentation structs

--- webserver/main.go
+++ webserver/main.go
@@ -1,13 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+const (
+	libraryRootFolder = "~/mockLib/"
+)
+
+type variable struct {
+	Name         string `json:"name"`
+	Description  string `json:"description"`
+	DefaultValue string `json:"defaultValue"`
+}
+
+type output struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+type module struct {
+	Name        string     `json:"name"`
+	Description string     `json:"description"`
+	Variables   []variable `json:"variables"`
+	Outputs     []output   `json:"outputs"`
+}
+
+type moduleList struct {
+	Modules []module `json:"modules"`
+}
+
+func parseModulesFromLibrary(rootFolder string) *moduleList {
+	var modules moduleList
+
+	//TODO: Run doctool to extract JSON representation for all  modules
+
+	//MOCK
+	modulesJSON, _ := ioutil.ReadFile("mockTerraform.json")
+	err := json.Unmarshal(modulesJSON, &modules)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(modules.Modules[0].Description)
+
+	return &modules
+}
+
 func main() {
+	parseModulesFromLibrary("a")
 	http.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		b, _ := ioutil.ReadFile("mockTerraform.json")
