9a3f9191e40bb889d497b03a480c0a9b106d4f66
Martin Jensen <martin.jensen.91@gmail.com>
2016-07-13 09:12:37 +0200

    refactor

--- webserver/src/main.go
+++ webserver/src/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"./api"
+	"./library"
 )
 
 func main() {
+	library.Init()
+	api.AddRoute("/api", func() []byte {
+		return []byte("{}")
+	})
+	api.AddRoute("/api/modules", library.GetModuleNames)
 	api.HandleRequests()
 }
