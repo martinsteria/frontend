9c050bddd49bcd4976947f33edae5d2566c6dadf
SOHOEL Halldis <halldis.sohoel@soprasteria.com>
2016-07-13 13:20:33 +0200

    kan laste opp fil, printer navn og størrelse

--- nettside/script.js
+++ nettside/script.js
@@ -117,3 +117,27 @@ function Milestone2(){
             varBesk4 = "Variabel for milestone 2";
             $("#innBesk4").html(varBesk4);
 }
+
+function LastOpp(){
+	var x = document.getElementById("myFile");
+    var txt = "";
+	if ('files' in x) {
+	//filen heter nå "file"
+	var file = x.files[0];
+	if ('name' in file) {
+		txt += "name: " + file.name + "<br>";
+		}
+	if ('size' in file) {
+		txt += "size: " + file.size + " bytes <br>";
+		}
+	}
+    else {
+        if (x.value == "") {
+            txt += "Select a file";
+        } else {
+            txt += "The files property is not supported by your browser!";
+            txt  += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead.
+        }
+    } 
+    document.getElementById("Fil").innerHTML = txt;
+}
