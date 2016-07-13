d6c3ddd798ff827780624bb53298825bb589b498
ENGELSEN Brynjar <brynjar.engelsen@soprasteria.com>
2016-07-13 14:02:19 +0200

    Fixed some bugs

--- webserver/src/Documentation/Documentation.go
+++ webserver/src/Documentation/Documentation.go
@@ -39,14 +39,18 @@ func BuildModule(path string) Module {
 	for _, f := range files {
 		if (strings.Contains(f.Name(), ".tf") && !strings.Contains(f.Name(), ".tfvars")){
 			file, err := os.Open(f.Name())
+
 			if err != nil {
 				log.Fatal(err)
 			}
+
 			scanner := bufio.NewScanner(file)
 			for scanner.Scan() {
 				line := scanner.Text()
+				line = strings.Replace(line, "\"", "", -1)
 				if strings.Contains(line, "#") {
-					newModule.Name = strings.Replace(line, "#", "", -1)
+					line = strings.Replace(line, "#", "", -1)
+					newModule.Name = strings.TrimSpace(line)
 				} else if strings.Contains(line, "Module Description") {
 					line := strings.Replace(line, "Module Description = ", "", -1)
 					for {
@@ -58,46 +62,51 @@ func BuildModule(path string) Module {
 						newModule.Description += strings.TrimSpace(line) //To get rid of blank enters.
 						scanner.Scan()
 						line = scanner.Text()
+						line = strings.Replace(line, "\"", "", -1)
 					}
 
 				} else if strings.Contains(line, "variable") {
-					line = strings.Trim(line, "variable { } ")
-					line = strings.Trim(line, "\"")
+					line = strings.Replace(line, "variable", "", -1)
+					line = strings.Replace(line, "\"", "", -1)
+					line = strings.Trim(line, " { } ")
 
 					var temp_variable variable
-					temp_variable.Name = line
+					temp_variable.Name = strings.TrimSpace(line)
 					for {
 						if strings.Contains(scanner.Text(), "}") {
 							break
 						}
 						scanner.Scan()
 						line = scanner.Text()
+						line = strings.Replace(line, "\"", "", -1)
 						if strings.Contains(line, "default") {
-							line = strings.Replace(line, "default = ", "", -1)
-							temp_variable.DefaultValue = strings.Trim(line, " \"")
+							line = strings.Replace(line, "default =", "", -1)
+							temp_variable.DefaultValue = strings.TrimSpace(line)
 						} else if strings.Contains(line, "description") {
-							line = strings.Trim(line, " description = ")
-							temp_variable.Description = strings.Trim(line, "\"")
+							line = strings.Replace(line, "description =", "", -1)						
+							temp_variable.Description = strings.TrimSpace(line)
 						}
 					}
 					variables = append(variables, temp_variable)
 
 				} else if strings.Contains(line, "output") {
 					var temp_output output
-					line = strings.Trim(line, "output { } ")
-					line = strings.Trim(line, "\"")
-					temp_output.Name = strings.Trim(line, "\"")
+					line = strings.Replace(line, "output", "", -1)
+					line = strings.Trim(line, " { } ")
+					temp_output.Name = strings.TrimSpace(line)
 					for {
-						if strings.Contains(scanner.Text(), "}") || strings.Contains(scanner.Text(), "*/") {
+						if strings.Contains(line, "}") || strings.Contains(scanner.Text(), "*/") {
 							add = false
 							break
 						} else if add {
-							temp_output.Description += strings.Trim(scanner.Text(), "Output Description = ")
-						} else if strings.Contains(scanner.Text(), "Output Description") {
+							temp_output.Description += strings.TrimSpace(line)
+						} else if strings.Contains(line, "Output Description") {
 							add = true
-							temp_output.Description += scanner.Text()
+							temp_output.Description += strings.Replace(line, "Output Description = ", "", -1)
 						}
 						scanner.Scan()
+						line = scanner.Text()
+						line = strings.Replace(line, "\"", "", -1)
 					}
 
 					outputs = append(outputs, temp_output)
