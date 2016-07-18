2d15ce72ebd34f8779c3addda2c22222c76d3493
Martin Jensen <martin.jensen.91@gmail.com>
2016-07-18 13:17:59 +0200

    moved users to requests

---
+++ webserver/src/requests/requests.go
@@ -0,0 +1,88 @@
+package requests
+
+import (
+	"api"
+	"doc"
+	"fmt"
+	"io/ioutil"
+	"library"
+	"terraform"
+	"user"
+)
+
+const (
+	usersRootDir = "/home/martin/users"
+)
+
+var users map[string]user.User
+var lib user.User
+
+func Init() {
+	users = make(map[string]user.User)
+	files, _ := ioutil.ReadDir(usersRootDir)
+	for _, u := range files {
+		userPath := usersRootDir + "/" + u.Name()
+		users[u.Name()] = user.User{userPath, library.BuildLibrary(userPath)}
+	}
+
+	lib = user.User{
+		library.LibraryModules,
+		library.BuildLibrary(library.LibraryModules)}
+}
+
+func HandleUserRequests(r api.RequestData) []byte {
+	if r.Method == "GET" {
+		if _, present := r.Query["add"]; present {
+			users[r.Query["add"]] = user.CreateUser(usersRootDir + "/" + r.Query["add"])
+		}
+
+		if _, present := r.Query["user"]; present {
+			user := users[r.Query["user"]]
+			if _, present := r.Query["get"]; present {
+				return user.Lib.GetModuleDocumentationJSON(r.Query["get"])
+			} else {
+				return user.Lib.GetModuleListJSON()
+			}
+		}
+
+	} else if r.Method == "POST" {
+		_, present := r.Query["user"]
+		if present {
+			user := users[r.Query["user"]]
+			_, present = r.Query["module"]
+			if present {
+				_, present = r.Query["terraform"]
+				if present {
+					modulePath := users[r.Query["user"]].Dir + "/" + r.Query["module"]
+					doc.CreateTFvars(modulePath, r.Body)
+					user.Lib.Modules[r.Query["module"]] = doc.ReadVariableValues(modulePath, user.Lib.Modules[r.Query["module"]])
+					fmt.Println(user.Lib.Modules[r.Query["module"]])
+					fmt.Println(terraform.TerraformCommand(r.Query["terraform"], modulePath))
+
+				}
+			}
+		}
+	}
+	return []byte("{}")
+}
+
+func HandleLibraryRequests(r api.RequestData) []byte {
+	if r.Method == "GET" {
+		v, present := r.Query["get"]
+
+		if present {
+			return lib.Lib.GetModuleDocumentationJSON(v)
+		}
+
+		v, present = r.Query["copy"]
+		if present {
+			v, present = r.Query["user"]
+			if present {
+				user := users[r.Query["user"]]
+				user.AddModule(library.LibraryModules + "/" + r.Query["copy"])
+			}
+		}
+	}
+
+	return lib.Lib.GetModuleListJSON()
+}
