ee5c3dba7239e56fa4679602638ffa1dfe0a4d81
Martin Jensen <martin.jensen.91@gmail.com>
2016-07-18 15:04:49 +0200

    cleanup. pointer maps in progress

--- webserver/src/requests/requests.go
+++ webserver/src/requests/requests.go
@@ -4,30 +4,16 @@ import (
 	"api"
 	"doc"
 	"fmt"
-	"io/ioutil"
 	"library"
 	"terraform"
-	"user"
+	"users"
 )
 
-const (
-	usersRootDir = "/home/martin/users"
-)
-
-var users map[string]user.User
-var lib user.User
+var lib library.Library
 
 func Init() {
-	users = make(map[string]user.User)
-	files, _ := ioutil.ReadDir(usersRootDir)
-	for _, u := range files {
-		userPath := usersRootDir + "/" + u.Name()
-		users[u.Name()] = user.User{userPath, library.BuildLibrary(userPath)}
-	}
-
-	lib = user.User{
-		library.LibraryModules,
-		library.BuildLibrary(library.LibraryModules)}
+	library.lib.Init(library.LibraryModules)
+	users.Init()
 }
 
 func HandleUserRequests(r api.RequestData) []byte {
@@ -36,53 +22,40 @@ func HandleUserRequests(r api.RequestData) []byte {
 			users[r.Query["add"]] = user.CreateUser(usersRootDir + "/" + r.Query["add"])
 		}
 
-		if _, present := r.Query["user"]; present {
-			user := users[r.Query["user"]]
-			if _, present := r.Query["get"]; present {
-				return user.Lib.GetModuleDocumentationJSON(r.Query["get"])
+		if user, present := r.Query["user"]; present {
+			if module, present := r.Query["module"]; present {
+				return users.GetLibrary(user).GetModuleDocumentationJSON(module)
 			} else {
-				return user.Lib.GetModuleListJSON()
+				return users.GetLibrary(user).GetModuleListJSON()
 			}
 		}
 
 	} else if r.Method == "POST" {
-		_, present := r.Query["user"]
-		if present {
-			user := users[r.Query["user"]]
-			_, present = r.Query["module"]
-			if present {
-				_, present = r.Query["terraform"]
-				if present {
-					modulePath := users[r.Query["user"]].Dir + "/" + r.Query["module"]
-					doc.CreateTFvars(modulePath, r.Body)
-					user.Lib.Modules[r.Query["module"]] = doc.ReadVariableValues(modulePath, user.Lib.Modules[r.Query["module"]])
-					fmt.Println(user.Lib.Modules[r.Query["module"]])
-					fmt.Println(terraform.TerraformCommand(r.Query["terraform"], modulePath))
-
-				}
-			}
+		if user, present := r.Query["user"]; present {
+			users.AddUser(user)
+			return []byte("{\"status\": \"success\"}")
 		}
 	}
-	return []byte("{}")
+
+	return users.GetUserListJSON()
 }
 
 func HandleLibraryRequests(r api.RequestData) []byte {
 	if r.Method == "GET" {
-		v, present := r.Query["get"]
-
-		if present {
-			return lib.Lib.GetModuleDocumentationJSON(v)
-		}
-
-		v, present = r.Query["copy"]
-		if present {
-			v, present = r.Query["user"]
-			if present {
-				user := users[r.Query["user"]]
-				user.AddModule(library.LibraryModules + "/" + r.Query["copy"])
-			}
+		if module, present := r.Query["module"]; present {
+			return lib.GetModuleDocumentationJSON(module)
 		}
 	}
 
-	return lib.Lib.GetModuleListJSON()
+	return lib.GetModuleListJSON()
+}
+
+func HandleLibraryCopyRequests(r api.RequestData) []byte {
+	if r.Method == "POST" {
+		if user, present := r.Query["user"]; present {
+			if module, present := r.Query["module"]; present {
+				users.AddModule(user, lib.GetRootDir()+"/"+module)
+			}
+		}
+	}
 }
