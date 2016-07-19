8536be859bfe19ba19976a15b08cda5f890118a5
Martin Jensen <martin.jensen@soprasteria.com>
2016-07-19 15:44:51 +0200

    fixing things in progress

--- nettside/script.js
+++ nettside/script.js
@@ -1,8 +1,18 @@
-﻿var user
+﻿//The current user operating the website
+var user
+
+//The current module the user is editing
 var module
-var modules = "http://52.169.232.92/api/library"
-var users = "http://52.169.232.92/api/users"
-var deployment = "http://52.169.232.92/api/deploy"
+
+//Is the module from the library or from the users collection
+//VALS: "USER" or "LIB"
+var moduleSource
+
+//API endpoints
+var apiRoot = "http://52.169.232.92/api"
+var modules = apiRoot + "/library"
+var users = apiRoot + "/users"
+var deployment = apiRoot + "/deploy"
 
 $(document).ready(function () {
     $.ajaxSetup({ cache: false })
@@ -27,7 +37,6 @@ function logIn() {
 	  $.get({
 		    url: users,
 		    success: function(result) {
-			      console.log(result)
 			      var e = false;
 			      for (i=0; i< result.length ; i++) {
 				        console.log(result[i]+"="+user);
@@ -36,7 +45,7 @@ function logIn() {
 				        }
 			      }
 			      if (e == true) {
-			          importLibraryModules(users + "?user=" + user, "existing", "&");
+			          importUserModules(users, user);
 			      }
 			      else {
 				        makeUser(user);
@@ -44,7 +53,7 @@ function logIn() {
 		    }
 	  })
     $("#login-view").fadeOut("slow", function() {
-        importLibraryModules(modules, "library", "?")
+        importLibraryModules(modules)
         $("#library-view").fadeIn("slow")
     })
 }
@@ -53,27 +62,42 @@ function makeUser(user) {
 	console.log("make new user")
 }
 
-/*
-x er enten lik '&' eller '?'
-users + "?user=" + user + "&module=" + module
-users + "?user=" + user + "?module=" + module
-*/
-function importLibraryModules(path, meny, x) {
+function importLibraryModules(path) {
     $.getJSON(path, function (resultModules) {
-        console.log(resultModules);
         var content = ""
         content += "<option selected disabled hidden>Biblioteksmoduler...</option>"
         for (i = 0; i < resultModules.length; i++) {
             content += "<option value=\"" + i + "\" id=\"" + resultModules[i].id + "\" >" + resultModules[i].name + "</option>"
         }
 
-        $("#"+meny).html(content);
-        $("#"+meny).change(function() {
-            module = $("#"+meny+" option:selected").text()
-            showModule(path + x +"module=" + module)
+        $("#library").html(content);
+        $("#library").change(function() {
+            module = $("#library option:selected").text()
+            moduleSource = "LIB"
+            showModule(path +"?module=" + module)
             $("#variables-view").fadeIn("slow")
         })
 
+    });
+}
+
+function importUserModules(path, user) {
+    $.getJSON(path + "?user=" + user, function (resultModules) {
+        console.log(resultModules)
+        console.log(resultModules);
+        var content = ""
+        content += "<option selected disabled hidden>Brukermoduler...</option>"
+        for (i = 0; i < resultModules.length; i++) {
+            content += "<option value=\"" + i + "\" id=\"" + resultModules[i].id + "\" >" + resultModules[i].name + "</option>"
+        }
+
+        $("#existing").html(content);
+        $("#existing").change(function() {
+            module = $("#existing option:selected").text()
+            moduleSource = "USER"
+            showModule(path + "?user=" + user +  "&module=" + module)
+            $("#variables-view").fadeIn("slow")
+        })
     });
 }
 
@@ -86,7 +110,13 @@ function showModule(path) {
         var myTable = ""
 				myTable += "<thead><tr><th>Navn</th><th>Verdi</th></tr></thead>"
         for (i = 0; i < result.variables.length; i++) {
-            var textInputBox = '<input type="text" class="form-control" value="' + result.variables[i].defaultValue + '" id="' + [i] + '" />';
+            console.log(result.variables[i])
+            var value = result.variables[i].value
+            if (value == "") {
+                value = result.variables[i].defaultValue
+            }
+
+            var textInputBox = '<input type="text" class="form-control" value="' + result.variables[i].defaultValue + '" id="' [i] + '" />';
             myTable += '<tr>'
             myTable += '<td><a href="#" data-placement="left" data-toggle="tooltip" title="' + result.variables[i].description + '">' + result.variables[i].name + '</a></td>'
             myTable += '<td>' + textInputBox + '</td>'
@@ -99,14 +129,8 @@ function showModule(path) {
 }
 
 function showDeployment() {
-    $.post({
-        url: modules + "/copy?" + "user=" + user + "&module=" + module,
-        success: function(result) {
-            console.log(result)
-        }
-    })
     $("#deploymentOutput").hide()
-    $("#deployment-view").show()
+    $("#deployment-view").fadeIn("slow")
     $("#planBtn").click(function() {
         deploy("plan")
     })
@@ -119,10 +143,20 @@ function showDeployment() {
 }
 
 function deploy(command) {
+    if (moduleSource == "LIB") {
+        $.post({
+            url: modules + "/copy?" + "user=" + user + "&module=" + module,
+            success: function(result) {
+                console.log(result)
+            }
+        })
+        importUserModules(users, user)
+    }
+
     var url = deployment + "?user=" + user + "&module=" + module + "&command=" + command
     $.post({
         url: url,
-        data: getParameters,
+        data: getParameters(),
         success: function(result) {
             console.log(result)
             showOutput()
@@ -133,7 +167,7 @@ function deploy(command) {
 }
 
 function showOutput() {
-    var url = deployment + "?user=" + user + "&module=" + module + "&command=plan"
+    var url = deployment + "?user=" + user + "&module=" + module
     $.getJSON(url, function(result) {
         $("#deploymentOutput").html(result.output)
         if (result.status == "Running") {
@@ -144,21 +178,14 @@ function showOutput() {
 }
 
 function getParameters() {
-	  var myRows = [];
-	  var $headers = $("th");
-	  var $rows = $("tbody tr").each(function(index) {
-	      $cells = $(this).find("td");
-	      myRows[index] = {};
-	      $cells.each(function(cellIndex) {
-		        if (cellIndex ==0){
-		            myRows[index]['name'] = $(this).html();
-		        }
-		        if (cellIndex ==2){
-		            var value = document.getElementById(index).value;
-		            myRows[index]['value'] = value;
-		        }
-	      });
-	  });
-	  var myObj = myRows;
-	  return JSON.stringify(myObj)
+    var table = document.getElementById("variablesTable");
+    var variables = []
+    for (var i = 1, row; row = table.rows[i]; i++) {
+        variables.push({
+            name: row.cells[0].childNodes[0].innerHTML,
+            value: row.cells[1].childNodes[0].value
+        })
+}
+    console.log(variables)
+	  return JSON.stringify(variables)
 };
