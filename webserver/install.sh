56ff9a68c77731f9a8176c444e9ac9d1637b2e01
Martin Jensen <martin.jensen.91@gmail.com>
2016-07-20 14:26:57 +0200

    updates install script

--- webserver/install.sh
+++ webserver/install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LIB_DIR="library"
+LIB_GIT_URL="https://github.com/martinsteria/library"
+USR_DIR="users"
+
 export GOPATH=$DIR
 mkdir ${DIR}/users
-git clone https://github.com/martinsteria/library ${DIR}/library
+if [ ! -d "${DIR}/library"]; then
+    git clone $LIB_GIT_URL ${DIR}/library
+fi
+
+sudo -E go run main.go 80
